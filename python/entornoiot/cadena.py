b154fb44aad5cf1af1389e5c1b080133a37ca7cc
aleferu <aleferu888@gmail.com>
2024-05-11 13:13:15 +0200

    Arreglados algunos errores y más tests

--- python/entornoiot/cadena.py
+++ python/entornoiot/cadena.py
@@ -4,80 +4,85 @@ from typing import Optional
 
 
 class ManejaTemperaturas(ABC):
-    temperaturas: list[float]
-    indice_actual: int
-    temperaturas_a_mantener: int
-    is_lista_entera: bool
-    manejador_temperaturas: Optional["ManejaTemperaturas"]
+    _temperaturas: list[float]
+    _indice_actual: int
+    _temperaturas_a_mantener: int
+    _is_lista_entera: bool
+    _manejador_temperaturas: Optional["ManejaTemperaturas"]
 
     def __init__(self, temperaturas_a_mantener: int, manejador_temperaturas: Optional["ManejaTemperaturas"] = None):
-        self.temperaturas = list()
-        self.indice_actual = 0
-        self.temperaturas_a_mantener = temperaturas_a_mantener
-        self.is_lista_entera = self.indice_actual == self.temperaturas_a_mantener
-        self.manejador_temperaturas = manejador_temperaturas
+        self._temperaturas = list()
+        self._indice_actual = 0
+        self._temperaturas_a_mantener = temperaturas_a_mantener
+        self._is_lista_entera = self._indice_actual == self._temperaturas_a_mantener
+        self._manejador_temperaturas = manejador_temperaturas
 
     def manejar_temperatura(self, nueva_temperatura: float):
-        if not self.is_lista_entera:
-            self.temperaturas.append(nueva_temperatura)
+        if not self._is_lista_entera:
+            self._temperaturas.append(nueva_temperatura)
         else:
-            self.temperaturas[self.indice_actual] = nueva_temperatura
-        self.indice_actual = (self.indice_actual + 1) % self.temperaturas_a_mantener
-        if self.indice_actual == 0 and not self.is_lista_entera:
-            self.is_lista_entera = True
+            self._temperaturas[self._indice_actual] = nueva_temperatura
+        self._indice_actual = (self._indice_actual + 1) % self._temperaturas_a_mantener
+        if self._indice_actual == 0 and not self._is_lista_entera:
+            self._is_lista_entera = True
 
 
 class CalculaEstadisticos(ManejaTemperaturas):
-    calculadoras_de_estadisticos: dict[str, CalculadoraEstadistico]
+    _calculadoras_de_estadisticos: dict[str, CalculadoraEstadistico]
 
-    def __init__(self, temperaturas_a_mantener: int):
-        super().__init__(temperaturas_a_mantener)
-        self.calculadoras_de_estadisticos = dict()
+    def __init__(self, temperaturas_a_mantener: int, manejador_temperaturas: Optional["ManejaTemperaturas"] = None):
+        super().__init__(temperaturas_a_mantener, manejador_temperaturas)
+        self._calculadoras_de_estadisticos = dict()
 
     def manejar_temperatura(self, nueva_temperatura: float):
         super().manejar_temperatura(nueva_temperatura)
-        for nombre, calculadora in self.calculadoras_de_estadisticos.items():
-            print(f"{nombre}\t: {round(calculadora.aplicar_alg(self.temperaturas), 3)}")
+        for nombre, calculadora in self._calculadoras_de_estadisticos.items():
+            print(f"{nombre}\t: {round(calculadora.aplicar_alg(self._temperaturas), 3)}")
 
-        if self.manejador_temperaturas:
-            self.manejador_temperaturas.manejar_temperatura(nueva_temperatura)
+        if self._manejador_temperaturas:
+            self._manejador_temperaturas.manejar_temperatura(nueva_temperatura)
 
     def nueva_calculadora(self, nombre: str, calculadora: CalculadoraEstadistico):
-        self.calculadoras_de_estadisticos[nombre] = calculadora
+        self._calculadoras_de_estadisticos[nombre] = calculadora
 
 
 class ComprobadorUmbral(ManejaTemperaturas):
-    umbral: float
+    _umbral: float
 
-    def __init__(self, temperaturas_a_mantener: int, umbral: float):
-        super().__init__(temperaturas_a_mantener)
-        self.umbral = umbral
+    def __init__(self, umbral: float, manejador_temperaturas: Optional["ManejaTemperaturas"] = None):
+        super().__init__(1, manejador_temperaturas)
+        self._umbral = umbral
 
     def manejar_temperatura(self, nueva_temperatura: float):
         super().manejar_temperatura(nueva_temperatura)
-        print(f"¿Supera la temperatura los {self.umbral} grados?", end=" ")
-        print("Sí" if self.temperaturas[0] > self.umbral else "No", end=".\n")
+        print(f"¿Supera la temperatura actual los {self._umbral} grados?", end=" ")
+        print("Sí" if self._temperaturas[0] > self._umbral else "No", end=".\n")
 
-        if self.manejador_temperaturas:
-            self.manejador_temperaturas.manejar_temperatura(nueva_temperatura)
+        if self._manejador_temperaturas:
+            self._manejador_temperaturas.manejar_temperatura(nueva_temperatura)
 
 
 class ComprobadorDelta(ManejaTemperaturas):
-    delta: float
+    _delta: float
 
-    def __init__(self, temperaturas_a_mantener: int, delta: float):
-        self.temperaturas = [0] * temperaturas_a_mantener
-        self.indice_actual = 0
-        self.temperaturas_a_mantener = temperaturas_a_mantener
-        self.is_lista_entera = self.indice_actual == self.temperaturas_a_mantener
-        self.delta = delta
+    def __init__(self, temperaturas_a_mantener: int, delta: float, manejador_temperaturas: Optional["ManejaTemperaturas"] = None):
+        self._temperaturas = [0] * temperaturas_a_mantener
+        self._indice_actual = 0
+        self._temperaturas_a_mantener = temperaturas_a_mantener
+        self._is_lista_entera = self._indice_actual == self._temperaturas_a_mantener
+        self._delta = delta
+        self._manejador_temperaturas = manejador_temperaturas
 
     def manejar_temperatura(self, nueva_temperatura: float):
-        super().manejar_temperatura(nueva_temperatura)
-        temperatura_a_comparar = self.temperaturas[0] if not self.is_lista_entera else self.temperaturas[self.indice_actual]
-        resultado = abs(nueva_temperatura - temperatura_a_comparar) > self.delta
-        print(f"En los últimos {self.temperaturas_a_mantener * 5} segundos, ha aumentado la temperatura {self.delta} grados?", end=" ")
+        self._temperaturas[self._indice_actual] = nueva_temperatura
+        self._indice_actual = (self._indice_actual + 1) % self._temperaturas_a_mantener
+        if self._indice_actual == 0 and not self._is_lista_entera:
+            self._is_lista_entera = True
+
+        temperatura_a_comparar = self._temperaturas[0] if not self._is_lista_entera else self._temperaturas[self._indice_actual]
+        resultado = nueva_temperatura - temperatura_a_comparar >= self._delta
+        print(f"En los últimos {(self._temperaturas_a_mantener - 1) * 5} segundos, ¿ha aumentado la temperatura {self._delta} grados?", end=" ")
         print("Sí" if resultado else "No", end=".\n")
 
-        if self.manejador_temperaturas:
-            self.manejador_temperaturas.manejar_temperatura(nueva_temperatura)
+        if self._manejador_temperaturas:
+            self._manejador_temperaturas.manejar_temperatura(nueva_temperatura)
---
+++ python/test_cadena.py
@@ -0,0 +1,198 @@
+from entornoiot import (
+    CalculaEstadisticos,
+    ComprobadorUmbral,
+    ComprobadorDelta,
+    CalculadoraMedia,
+    CalculadoraCuasiVar
+)
+
+
+######################
+# ManejaTemperaturas #
+######################
+
+
+# CalculaEstadisticos
+
+
+def test_calcula_estadisticos_inicializado_correctamente():
+    objeto0 = CalculaEstadisticos(2)
+    assert len(objeto0._temperaturas) == 0
+    assert objeto0._indice_actual == 0
+    assert objeto0._temperaturas_a_mantener == 2
+    assert not objeto0._is_lista_entera
+    assert not objeto0._manejador_temperaturas
+    assert len(objeto0._calculadoras_de_estadisticos) == 0
+
+    objeto1 = CalculaEstadisticos(4, objeto0)
+    assert objeto1._manejador_temperaturas is objeto0
+
+
+# Al parecer capsys se usa para capturar el output de un test
+# Fuente: https://docs.pytest.org/en/7.1.x/how-to/capture-stdout-stderr.html
+def test_calcula_estadisticos_correctamente(capsys):
+    calcula_estadisticos = CalculaEstadisticos(2)
+    calcula_estadisticos.nueva_calculadora("Media", CalculadoraMedia())
+    calcula_estadisticos.nueva_calculadora("Varianza", CalculadoraCuasiVar())
+    assert len(calcula_estadisticos._calculadoras_de_estadisticos) == 2
+    assert isinstance(calcula_estadisticos._calculadoras_de_estadisticos["Media"], CalculadoraMedia)
+    assert isinstance(calcula_estadisticos._calculadoras_de_estadisticos["Varianza"], CalculadoraCuasiVar)
+
+    calcula_estadisticos.manejar_temperatura(1)
+    out, _ = capsys.readouterr()
+    assert out == "Media\t: 1.0\nVarianza\t: 0\n"
+    assert calcula_estadisticos._temperaturas == [1]
+
+    calcula_estadisticos.manejar_temperatura(2)
+    out, _ = capsys.readouterr()
+    assert out == "Media\t: 1.5\nVarianza\t: 0.5\n"
+    assert calcula_estadisticos._is_lista_entera
+    assert calcula_estadisticos._temperaturas == [1, 2]
+
+    calcula_estadisticos.manejar_temperatura(3)
+    out, _ = capsys.readouterr()
+    assert out == "Media\t: 2.5\nVarianza\t: 0.5\n"
+    assert calcula_estadisticos._temperaturas == [3, 2]
+
+
+def test_calcula_estadisticos_cadena(capsys):
+    calcula_estadisticos0 = CalculaEstadisticos(2)
+    calcula_estadisticos0.nueva_calculadora("Varianza", CalculadoraCuasiVar())
+
+    calcula_estadisticos1 = CalculaEstadisticos(3, calcula_estadisticos0)
+    calcula_estadisticos1.nueva_calculadora("Media", CalculadoraMedia())
+
+    calcula_estadisticos1.manejar_temperatura(1)
+    out, _ = capsys.readouterr()
+    assert out == "Media\t: 1.0\nVarianza\t: 0\n"
+
+    calcula_estadisticos1.manejar_temperatura(2)
+    out, _ = capsys.readouterr()
+    assert out == "Media\t: 1.5\nVarianza\t: 0.5\n"
+
+    calcula_estadisticos1.manejar_temperatura(3)
+    out, _ = capsys.readouterr()
+    assert out == "Media\t: 2.0\nVarianza\t: 0.5\n"
+
+
+# ComprobadorUmbral
+
+
+def test_comprobador_umbral_inicializado_correctamente():
+    objeto0 = ComprobadorUmbral(5)
+    assert len(objeto0._temperaturas) == 0
+    assert objeto0._indice_actual == 0
+    assert objeto0._temperaturas_a_mantener == 1
+    assert not objeto0._is_lista_entera
+    assert not objeto0._manejador_temperaturas
+    assert objeto0._umbral == 5
+
+    objeto1 = ComprobadorUmbral(4, objeto0)
+    assert objeto1._manejador_temperaturas is objeto0
+
+
+def test_comprobador_umbral(capsys):
+    umbral = 5
+    comprobador_umbral = ComprobadorUmbral(umbral)
+
+    comprobador_umbral.manejar_temperatura(1)
+    out, _ = capsys.readouterr()
+    assert out == f"¿Supera la temperatura actual los {umbral} grados? No.\n"
+    assert comprobador_umbral._is_lista_entera
+
+    comprobador_umbral.manejar_temperatura(5)
+    out, _ = capsys.readouterr()
+    assert out == f"¿Supera la temperatura actual los {umbral} grados? No.\n"
+
+    comprobador_umbral.manejar_temperatura(6)
+    out, _ = capsys.readouterr()
+    assert out == f"¿Supera la temperatura actual los {umbral} grados? Sí.\n"
+
+
+def test_comprobador_umbral_cadena(capsys):
+    umbral0 = 2
+    comprobador_umbral0 = ComprobadorUmbral(umbral0)
+    umbral1 = 3
+    comprobador_umbral1 = ComprobadorUmbral(umbral1, comprobador_umbral0)
+
+    comprobador_umbral1.manejar_temperatura(1.5)
+    out, _ = capsys.readouterr()
+    assert out == f"¿Supera la temperatura actual los {umbral1} grados? No.\n" +\
+        f"¿Supera la temperatura actual los {umbral0} grados? No.\n"
+
+    comprobador_umbral1.manejar_temperatura(2.5)
+    out, _ = capsys.readouterr()
+    assert out == f"¿Supera la temperatura actual los {umbral1} grados? No.\n" +\
+        f"¿Supera la temperatura actual los {umbral0} grados? Sí.\n"
+
+    comprobador_umbral1.manejar_temperatura(3.5)
+    out, _ = capsys.readouterr()
+    assert out == f"¿Supera la temperatura actual los {umbral1} grados? Sí.\n" +\
+        f"¿Supera la temperatura actual los {umbral0} grados? Sí.\n"
+
+
+# ComprobadorDelta
+
+
+def test_comprobador_delta_inicializado_correctamente():
+    objeto0 = ComprobadorDelta(3, 5)
+    assert len(objeto0._temperaturas) == 3
+    assert objeto0._indice_actual == 0
+    assert objeto0._temperaturas_a_mantener == 3
+    assert not objeto0._is_lista_entera
+    assert not objeto0._manejador_temperaturas
+    assert objeto0._delta == 5
+
+    objeto1 = ComprobadorDelta(4, 6, objeto0)
+    assert objeto1._manejador_temperaturas is objeto0
+
+
+def test_comprobador_delta(capsys):
+    temperaturas_a_mantener = 2
+    delta = 5
+    comprobador_delta = ComprobadorDelta(temperaturas_a_mantener, delta)
+
+    comprobador_delta.manejar_temperatura(1)
+    out, _ = capsys.readouterr()
+    assert out == f"En los últimos {(temperaturas_a_mantener - 1) * 5} segundos, ¿ha aumentado la temperatura {delta} grados? No.\n"
+    assert comprobador_delta._temperaturas == [1, 0]
+
+    comprobador_delta.manejar_temperatura(6)
+    out, _ = capsys.readouterr()
+    assert out == f"En los últimos {(temperaturas_a_mantener - 1) * 5} segundos, ¿ha aumentado la temperatura {delta} grados? Sí.\n"
+    assert comprobador_delta._is_lista_entera
+    assert comprobador_delta._temperaturas == [1, 6]
+
+    comprobador_delta.manejar_temperatura(5)
+    out, _ = capsys.readouterr()
+    assert out == f"En los últimos {(temperaturas_a_mantener - 1) * 5} segundos, ¿ha aumentado la temperatura {delta} grados? No.\n"
+    assert comprobador_delta._temperaturas == [5, 6]
+
+    comprobador_delta.manejar_temperatura(15)
+    out, _ = capsys.readouterr()
+    assert out == f"En los últimos {(temperaturas_a_mantener - 1) * 5} segundos, ¿ha aumentado la temperatura {delta} grados? Sí.\n"
+    assert comprobador_delta._temperaturas == [5, 15]
+
+
+def test_comprobador_delta_cadena(capsys):
+    temperaturas_a_mantener0 = 2
+    delta0 = 2
+    comprobador_delta0 = ComprobadorDelta(temperaturas_a_mantener0, delta0)
+    temperaturas_a_mantener1 = 3
+    delta1 = 3
+    comprobador_delta1 = ComprobadorDelta(temperaturas_a_mantener1, delta1, comprobador_delta0)
+
+    comprobador_delta1.manejar_temperatura(0.5)
+    out, _ = capsys.readouterr()
+    assert out == f"En los últimos {(temperaturas_a_mantener1 - 1) * 5} segundos, ¿ha aumentado la temperatura {delta1} grados? No.\n" +\
+        f"En los últimos {(temperaturas_a_mantener0 - 1) * 5} segundos, ¿ha aumentado la temperatura {delta0} grados? No.\n"
+
+    comprobador_delta1.manejar_temperatura(2.5)
+    out, _ = capsys.readouterr()
+    assert out == f"En los últimos {(temperaturas_a_mantener1 - 1) * 5} segundos, ¿ha aumentado la temperatura {delta1} grados? No.\n" +\
+        f"En los últimos {(temperaturas_a_mantener0 - 1) * 5} segundos, ¿ha aumentado la temperatura {delta0} grados? Sí.\n"
+
+    comprobador_delta1.manejar_temperatura(15)
+    out, _ = capsys.readouterr()
+    assert out == f"En los últimos {(temperaturas_a_mantener1 - 1) * 5} segundos, ¿ha aumentado la temperatura {delta1} grados? Sí.\n" +\
+        f"En los últimos {(temperaturas_a_mantener0 - 1) * 5} segundos, ¿ha aumentado la temperatura {delta0} grados? Sí.\n"
